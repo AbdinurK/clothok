b537888fb0d1263f2508a762c49bca86548b240d
AbdinurK <43904227+AbdinurK@users.noreply.github.com>
2020-02-24 00:01:31 +0600

    adding connection db

---
+++ server/config/key.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MONGO_URI: 'mongodb+srv://abdi:1234@cluster0-2z6og.mongodb.net/test?retryWrites=true&w=majority'
+};
--- server/index.js
+++ server/index.js
@@ -1,1 +1,20 @@
 const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = require('./config/key').MONGO_URI;
+
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
+    .then(() => console.log("MongoDB connected successfully..."))
+    .catch(err => console.log(err));
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
