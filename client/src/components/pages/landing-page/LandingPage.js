9ab033a7337fc1c229af92d5dc80f21bed3a12a6
Kuatbek Abdinur <kuatbekabdinur@gmail.com>
2020-07-21 02:02:48 +0600

    adaptive

--- client/src/components/pages/landing-page/LandingPage.js
+++ client/src/components/pages/landing-page/LandingPage.js
@@ -19,10 +19,10 @@ export default class LandingPage extends Component {
 
     render() {
         return (
-            <main>
+            <main className="landing">
                 <section className="section">
                     <div className="section__first">
-                        <div className="container">
+                        <div className="section__container">
                             <nav className="navigation">
                                 <a href="#" className="brand-logo">Clothok</a>
                                 <ul className="nav-list">
@@ -50,7 +50,7 @@ export default class LandingPage extends Component {
                     <div className="section__second">
                         <div className="section__info">
                             <h1 className="section__title">Design your stuff</h1>
-                            <Link to="/design-lab" className="section__action">Try it</Link>
+                            <Link to="/design-lab" className="section__action">Try</Link>
                         </div>
                         <img src={design} className="design-img" alt="design-img"/>
                     </div>
@@ -61,6 +61,37 @@ export default class LandingPage extends Component {
                         <div className="section__info">
                             <h1 className="section__title">Our clients</h1>
                         </div>
+                        <div className="section__content">
+                            <div className="testimonial">
+                                <div className="testimonial__avatar">
+                                </div>
+                                <p className="testimonial__text">
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. A gravida a nibh tortor
+                                    ante. Nunc, mattis tempor, in tellus.
+                                </p>
+                                <a className="testimonial__link">@Mike</a>
+                            </div>
+                            <div className="testimonial">
+                                <div className="testimonial__avatar">
+
+                                </div>
+                                <p className="testimonial__text">
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. A gravida a nibh tortor
+                                    ante. Nunc, mattis tempor, in tellus.
+                                </p>
+                                <a className="testimonial__link">@Mike</a>
+                            </div>
+                            <div className="testimonial">
+                                <div className="testimonial__avatar">
+
+                                </div>
+                                <p className="testimonial__text">
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. A gravida a nibh tortor
+                                    ante. Nunc, mattis tempor, in tellus.
+                                </p>
+                                <a className="testimonial__link">@Mike</a>
+                            </div>
+                        </div>
                     </div>
                 </section>
 
@@ -69,9 +100,17 @@ export default class LandingPage extends Component {
                         <div className="section__info">
                             <h1 className="section__title">About us</h1>
                         </div>
+                        <div className="section__content">
+                            <p className="section__text">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                Tristique in et, lobortis blandit nunc viverra. Sit vitae massa volutpat tincidunt
+                                aliquam, in lectus quam risus. Pellentesque sit ullamcorper imperdiet ac dictum
+                                lobortis nulla. Sapien praesent sit ultrices tincidunt.
+                            </p>
+                        </div>
                     </div>
                 </section>
-                 <Footer/>
+
                  <button id="scrollToTop" onClick={this.onScrollToTop}>
                      <span>Go up</span>
                      <img src={arrow1} alt="arrow 1" className="arrow"/>
